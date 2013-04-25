e399beaf46d1145135be6780492d48cd66b37ce5
omochimetaru <omochi.metaru@gmail.com>
2013-04-25 19:46:46 +0900

    static library build

---
+++ Objective-Zip/Objective-Zip.h
@@ -0,0 +1,14 @@
+//
+//  Objective-Zip.h
+//  Objective-Zip
+//
+//  Created by omochimetaru on 13/04/25.
+//
+//
+
+#import <Objective-Zip/ZipFile.h>
+#import <Objective-Zip/ZipException.h>
+#import <Objective-Zip/ZipWriteStream.h>
+#import <Objective-Zip/ZipReadStream.h>
+#import <Objective-Zip/FileInZipInfo.h>
+
